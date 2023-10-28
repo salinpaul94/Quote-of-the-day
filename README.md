f53b5cf00a1284f6852ef711b5da052bfd12fcad
salinpaul valooran <salinpv@hotmail.com>
2023-10-29 01:25:16 +0530

    async function to fetch data from api

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Quote-of-the-day
+https://github.com/lukePeavey/quotable
+
+api link
+https://api.quotable.io/quotes/random
+
+Get random quote try in browser
+
+GET /quotes/random
---
+++ version_01/index.js
@@ -0,0 +1,8 @@
+const api_url = "https://api.quotable.io/quotes/random";
+
+async function getQuote(url) {
+  const response = await fetch(url);
+  var data = await response.json();
+  console.log(data[0].content)
+}
+getQuote(api_url);
