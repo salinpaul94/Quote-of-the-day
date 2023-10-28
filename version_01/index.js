ee20cd86cf5c99e21e37ad876ff270d3bdc76974
salinpaul valooran <salinpv@hotmail.com>
2023-10-29 01:29:27 +0530

    adding quote to html page

--- version_01/index.js
+++ version_01/index.js
@@ -3,7 +3,7 @@ const api_url = "https://api.quotable.io/quotes/random";
 async function getQuote(url) {
   const response = await fetch(url);
   var data = await response.json();
-  console.log(data[0].content)
+  console.log(data);
 }
 
 window.addEventListener("DOMContentLoaded", getQuote(api_url));
