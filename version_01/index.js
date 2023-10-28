756432643e8469d5de29c1ce41888a3ebca20926
salinpaul valooran <salinpv@hotmail.com>
2023-10-29 01:32:52 +0530

    dynamically fetching quotes and loading on web page

--- version_01/index.js
+++ version_01/index.js
@@ -1,9 +1,11 @@
 const api_url = "https://api.quotable.io/quotes/random";
-
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
 async function getQuote(url) {
   const response = await fetch(url);
   var data = await response.json();
-  console.log(data);
+  quote.innerHTML = data[0].content;
+  author.innerHTML = data[0].author;
 }
 
 window.addEventListener("DOMContentLoaded", getQuote(api_url));
